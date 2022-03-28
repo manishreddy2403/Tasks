d0865c7bdcf27c667ccef75b33e44a2b0d86aec3
unknown <manish.amberpeta@wavemaker.com>
2022-03-28 14:25:18 +0530

    bank task added

---
+++ java/Account.java
@@ -0,0 +1,16 @@
+package com.banking;
+
+ public class Account{
+  String name;
+          Integer balance;
+          int depositCount = 0;
+          int withdrawCount = 0;
+
+public Account(String name, Integer balance) {
+        this.name = name;
+        this.balance = balance;
+        this.depositCount = 0;
+        this.withdrawCount = 0;
+        }
+        }
+
